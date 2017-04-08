42c79c0057dc7e67e4c422de29a69d95e265ae9d
Roger Diller <roger@flexrentalsolutions.com>
2017-04-08 14:25:10 -0400

    added Person class and new /person GetMapping

--- src/main/kotlin/com/kotlin/HelloController.kt
+++ src/main/kotlin/com/kotlin/HelloController.kt
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
 
+    //The "fun" keyword declares a function. You can directly assign a result to the function as is done here.
     @GetMapping("")
     fun sayHello() = "Hello From Kotlin & Spring Boot!"
+
+    //There is no "new" operator in Kotlin. You invoke the function like a function call.
+    @GetMapping("/person")
+    fun person() = Person("Roger", "Diller")
 
 }
---
+++ src/main/kotlin/com/kotlin/Person.kt
@@ -0,0 +1,10 @@
+package com.kotlin
+
+/**
+ * Created by Roger Diller on 4/8/17.
+ */
+
+class Person(
+    var firstName: String,
+    var lastName: String
+)
